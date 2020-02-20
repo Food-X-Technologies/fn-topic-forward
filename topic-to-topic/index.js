f2a847dc17b21bfcbfefa087e1352aee34f448bf
Jef King <jef@spud.ca>
2020-02-20 15:30:00 -0800

    fixing

--- topic-to-topic/index.js
+++ topic-to-topic/index.js
@@ -1,13 +1,13 @@
-module.exports = function (context, model) {
+module.exports = function (context, message) {
     let brokeredMessage = {
-        body: JSON.stringify(model),
+        body: JSON.stringify(message),
         customProperties: {
             //Add custom properties to filter on with Subscriptions
         }
     }
 
-    let serviceBusService = azure.createServiceBusService(process.env.ServiceBus);
-    serviceBusService.sendTopicMessage(process.env.TopicName, brokeredMessage, function (error) {
+    let serviceBusService = azure.createServiceBusService(process.env.ServiceBusTo);
+    serviceBusService.sendTopicMessage(process.env.TopicTo, brokeredMessage, function (error) {
         context.done(error);
     });
 };
